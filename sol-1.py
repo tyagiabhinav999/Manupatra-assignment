66d8bfaec8941971a78b4f92a752723ec438dbaf
tyagiabhinav999 <tyagiabhinav999@gmail.com>
2025-05-16 21:36:23 +0530

    solved scrub-text

--- sol-1.py
+++ sol-1.py
@@ -1,13 +1,13 @@
 import re
 
 def scrub_text(text):
-    # HTML tags
+    # html tags
     text = re.sub(r'<[^>]+>', '', text)
 
     # email addresses
     text = re.sub(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', '', text)
 
-    # URLs
+    # for urls
     text = re.sub(r'https?://\S+|www\.\S+', '', text)
 
     return text
