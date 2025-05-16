eee93f250072e5153d6e257220ab4622d1099716
tyagiabhinav999 <tyagiabhinav999@gmail.com>
2025-05-16 21:41:47 +0530

    sample example added

--- sol-1.py
+++ sol-1.py
@@ -11,3 +11,23 @@ def scrub_text(text):
     text = re.sub(r'https?://\S+|www\.\S+', '', text)
 
     return text
+
+sample = """
+<p>paragraph with an <a href="http://example.com"> link</a>.</p>
+something hi@example.com website www.xyz.com.
+<div>html <b>bold text</b> and div </div>
+blablabla is https://sub.domain.com/path?query=value.
+"""
+
+out = scrub_text(sample)
+
+
+print(out)
+
+### Output is
+
+# PS D:\ManuPatra> python sol-1.py
+# paragraph with an  link.
+# something  website
+# html bold text and div
+# blablabla is
